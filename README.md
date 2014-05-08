f4ff5a1c9afb733104fc5b76bbb9b073c2fbb9db
Devin Rousso <dcrousso@gmail.com>
2014-05-07 20:41:08 -0700

    Added direction and leaveNoTrace options

--- README.md
+++ README.md
@@ -3,12 +3,13 @@ from the basic typewriter effect to fading drop-down letters
 
 ## Download
  Get the [raw](https://raw.github.com/dcrousso/jquery.typimate.js/master/jquery.typimate.js) script, download the complete [package](https://github.com/dcrousso/jquery.typimate.js/zipball/master) or fork it on [GitHub](https://github.com/dcrousso/jquery.typimate.js/).
+ [Let me know](http://devinrousso.com/contact) if you used this plugin and had fun with it.
 
 
 ## Installation
 Add the following script before your closing ```</body>``` tag:
 
-```<script src="jquery.typimate.js"/></script>```
+```<script src="jquery.typimate.min.js"></script>```
 
 
 ## Options
@@ -16,9 +17,11 @@ Add the following script before your closing ```</body>``` tag:
 Option | Type | Default | Description | Values
 ------ | ---- | ------- | ----------- | ------
 animation | string | "fade" | Determines the type of animation effect | "fade", "none"
+direction | string | "forward" | Direction that animation travels | "forward", "backward", "ends", "middle"
 duration | int | 400 | How long each animation takes | Integer
 easing | string | "in" | The direction in which the animation progresses | "in", "out", "inOut"
 interval | int | 100 | Time between each letter animation | Integer
+leaveNoTrace | boolean | true | Toggles whether element is set back to original form | true, false
 onComplete | function | $.noop | Function that runs once the animation is complete | Function
 onLetter | function | $.noop | Function that runs after each letter animation | Function
 timeout | int | 0 | Timeout before starting the animation | Integer
--- jquery.typimate.js
+++ jquery.typimate.js
@@ -25,15 +25,20 @@ SOFTWARE.
 	$.fn.typimate = function ( options ) {
 		var settings = {
 			animation: "fade",
+			direction: "forward",
 			duration: 400,
 			easing: "in",
 			interval: 100,
+			leaveNoTrace: true,
 			onComplete: $.noop,
 			onLetter: $.noop,
 			timeout: 0
 		};
 		if (options) $.extend(settings, options);
 		var target = $(this);
+		var html = target.html();
+		var animation;
+		var counter;
 
 		function splitTextIntoChars(e) {
 			var element = $(e);
@@ -47,12 +52,13 @@ SOFTWARE.
 		function getChars(e) {
 			var element = $(e);
 			var children = element.children();
-			setAnimation(children);
+			animation = setAnimation(children);
+			counter = setCounter(children);
 			return children;
 		}
-		function typeChar(e) {
+		function typeChar(e, callback) {
 			var element = $(e);
-			animation(element);
+			animation(element, callback);
 		}
 		function setAnimation(e) {
 			if(settings.animation == "none") {
@@ -62,41 +68,84 @@ SOFTWARE.
 			}
 			if(settings.easing == "out") {
 				e.css({"opacity":1});
-				animation = outAnimation;
+				return outAnimation;
 			} else if(settings.easing == "inOut") {
 				e.css({"opacity":0});
-				animation = inOutAnimation;
+				return inOutAnimation;
 			} else {
 				e.css({"opacity":0});
-				animation = inAnimation;
+				return inAnimation;
 			}
 		}
-		function animation(e) {}
-		function inAnimation(e){
-			e.animate({"opacity":1}, settings.duration);
+		function inAnimation(e, callback){
+			e.animate({"opacity":1}, settings.duration, callback);
 		}
-		function outAnimation(e){
-			e.animate({"opacity":0}, settings.duration);
+		function outAnimation(e, callback){
+			e.animate({"opacity":0}, settings.duration, callback);
 		}
-		function inOutAnimation(e){
-			inAnimation(e);
+		function inOutAnimation(e, callback){
+			inAnimation(e, $.noop);
 			setTimeout(function(){
-				outAnimation(e);
+				outAnimation(e, callback);
 			}, settings.timeout + settings.interval);
+		}
+		function setCounter(e){
+			if(settings.direction == "backward") {
+				return {
+					count: e.length - 1,
+					end: e.length - 1,
+					dir: -1,
+					mirror: false
+				};
+			} else if (settings.direction == "ends") {
+				return {
+					count: 0,
+					end: Math.floor((e.length + 6) / 2),
+					dir: 1,
+					mirror: true
+				};
+			} else if (settings.direction == "middle") {
+				return {
+					count: Math.round(e.length / 2),
+					end: e.length,
+					dir: -1,
+					mirror: true
+				};
+			} else {
+				return {
+					count: 0,
+					end: e.length - 1,
+					dir: 1,
+					mirror: false
+				};
+			}
 		}
 		
 		splitTextIntoChars(target);
 		var spans = getChars(target);
-		var index = 0;
+		var completed = 0;
+		var first = counter.count;
 		setTimeout(function() {
 			var interval = setInterval(function(){
-				typeChar(spans[index]);
-				index++;
-				settings.onLetter();
-				if(index == spans.length) {
-					clearInterval(interval);
-					settings.onComplete();
+				var complete = function() {
+					settings.onLetter();
+					if(completed >= counter.end) {
+						clearInterval(interval);
+						settings.onComplete();
+						if(settings.leaveNoTrace) {
+							if($(spans[first]).css("opacity") == 0) {
+								target.css({"opacity":0});
+							}
+							target.html(html);
+						}
+					}
+					completed ++;
 				}
+				typeChar(spans[counter.count], complete);
+				if(counter.mirror){
+					typeChar(spans[spans.length - 1 - counter.count], complete);
+				}
+				counter.count += counter.dir;
 			}, settings.interval);
 		}, settings.timeout);
 
